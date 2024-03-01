346268a76af6bc0a4c92a7b9a839888e7f9cb8bd
VANESSA FRANCISCO <VVLOERA36@GMAIL.COM>
2024-03-01 12:30:42 -0600

    updating my javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+console.log("is this thing working");
+alert("Hi, I am glad you are alive");
+
+const usersName = prompt("What is your name?");
+console.log(usersName);
+
+
+document.write("Hi hi" + usersName + "make yourself at home");
+
+
+let color = prompt('if my favorite color rhymes with mean, what color is it?');
+let message;
+
+
+if(color == green){
+   message = 'yup, correct';
+} else if ('red, blue, orange,yellow, pink'){
+   message = ('ehh, not quite');
+}
+
