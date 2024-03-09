eb19c883c0d1be4ac9839fb1c80d811649480c41
VANESSA FRANCISCO <VVLOERA36@GMAIL.COM>
2024-03-09 17:43:08 -0600

    more changes once again

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ const usersName = prompt("What is your name?");
 console.log(usersName);
 
 
+if (usersName == "aurelius"){
+  document.write("Hi hubs");
+} else if (usersName == "Damaris"){
+  document.write("hi bestie");
+} else if (usersName == "Cameron"){
+  document.write("I hope I'm doing this Javascript thing correct");
+} else {
+  document.write("Thanks for stopping by");
+}
+
 document.write("Hi hi, " + usersName + "make yourself at home");
 
 
-if (usersName == "aurelius"){
-   document.write("Hi hubs");
- } else if (usersName == "Damaris"){
-   document.write("hi bestie");
- } else if (usersName == "Cameron"){
-   document.write("I hope I'm doing this Javascript thing correct");
- } else {
-   document.write("Thanks for stopping by");
- }
+
 
 
