dd9d380e86bd048a9e8759ad19ce419421c6c2af
VANESSA FRANCISCO <VVLOERA36@GMAIL.COM>
2024-03-09 17:13:49 -0600

    making changes to a better grade

--- app.js
+++ app.js
@@ -4,20 +4,23 @@
 console.log("is this thing working");
 alert("Hi, I am glad you are alive");
 
+let color = prompt('if my favorite color rhymes with mean, what color is it?');
+
 const usersName = prompt("What is your name?");
 console.log(usersName);
 
 
-document.write("Hi hi" + usersName + "make yourself at home");
+document.write("Hi hi, " + usersName + "make yourself at home");
 
 
-let color = prompt('if my favorite color rhymes with mean, what color is it?');
-let message;
+if (usersName == "aurelius"){
+   document.write("Hi hubs");
+ } else if (usersName == "Damaris"){
+   document.write("hi bestie");
+ } else if (usersName == "Cameron"){
+   document.write("I hope I'm doing this Javascript thing correct");
+ } else {
+   document.write("Thanks for stopping by");
+ }
 
-
-if(color == green){
-   message = 'yup, correct';
-} else if ('red, blue, orange,yellow, pink'){
-   message = ('ehh, not quite');
-}
 
