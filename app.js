dea44c57f0b6c88b11b0bf8536faed78b5bc3e79
VANESSA FRANCISCO <VVLOERA36@GMAIL.COM>
2024-03-19 14:28:58 -0500

    third try at functions

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict';
 
 
-//console.log("is this thing working");
-//alert("Hi, I am glad you are alive");
+console.log("is this thing working");
+alert("Hi, I am glad you are alive");
 
 function getAnswer(color){
-  let color = prompt('if my favorite color rhymes with mean, what color is it?');
-  if (color = ""){
-    color = prompt("Come on, you know it!");
+  const color = prompt('if my favorite color rhymes with mean, what color is it?');
+  if (color == "green"){
+  if (color) == "" prompt("Come on, you know it!");
   }
 }
 
@@ -21,19 +21,13 @@ console.log(usersName);
 function sayingHello(){
   document.write("Hi hi, " + usersName);
 }
-function sayingHello(userName){
-  if (userName ==""){
-    userName = promopt("Okay then, no name");
-  }
-}
+
 if (usersName == "aurelius"){
   document.write("Hi hubs");
 } else if (usersName == "Damaris"){
   document.write("hi bestie");
 } else if (usersName == "Cameron"){
   document.write("I hope I'm doing this Javascript thing correct");
-} else {
-  document.write("Thanks for stopping by");
 }
 
 
